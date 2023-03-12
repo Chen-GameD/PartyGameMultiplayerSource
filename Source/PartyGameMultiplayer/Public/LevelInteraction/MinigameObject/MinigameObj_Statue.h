d15f0b72d66f94f7073f4c3ac9f5518c71818217
CMY <chen.my.gamed@gmail.com>
2023-03-11 18:02:05 -0700

    new class for statue

---
+++ Source/PartyGameMultiplayer/Public/LevelInteraction/MinigameObject/MinigameObj_Statue.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LevelInteraction/MinigameMainObjective.h"
+#include "MinigameObj_Statue.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARTYGAMEMULTIPLAYER_API AMinigameObj_Statue : public AMinigameMainObjective
+{
+	GENERATED_BODY()
+
+public:
+	AMinigameObj_Statue();
+	
+};
