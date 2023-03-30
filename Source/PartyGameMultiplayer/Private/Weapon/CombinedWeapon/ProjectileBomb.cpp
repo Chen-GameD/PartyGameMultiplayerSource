b4fc52fc6e6e51f3d8d380588bfd8b0df64d8728
AmosTLOU <didala2009@163.com>
2023-03-30 00:13:52 -0600

    improve alarm and bomb hit effect

--- Source/PartyGameMultiplayer/Private/Weapon/CombinedWeapon/ProjectileBomb.cpp
+++ Source/PartyGameMultiplayer/Private/Weapon/CombinedWeapon/ProjectileBomb.cpp
@@ -85,7 +85,7 @@ void AProjectileBomb::OnRep_HasExploded()
 	if (HasExploded)
 	{
 		ProjectileMovementComponent->StopMovementImmediately();  // will stop the original movement, but will still move the object(like a free fall)
-		ProjectileMovementComponent->SetUpdatedComponent(nullptr);
+		ProjectileMovementComponent->SetUpdatedComponent(nullptr); // ProjectileMovementComponent will stop moving the object
 		StaticMesh->SetSimulatePhysics(false);
 		StaticMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
